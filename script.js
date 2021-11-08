5da22bd27569feb4509918388b753f1f38493c4a
MTheuws <89788123+MTheuws@users.noreply.github.com>
2021-11-08 15:33:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//deel1
+const myAge = 20;
+if (myAge > 17) {
+    document.write("Je voldoet aan de minimumleeftijd en bent van harte welkom");
+} else {
+    document.write("Sorry, je voldoet helaas niet aan de minimum leeftijd en je wordt vriendelijk verzocht niet binnen te komen");
+}
+
+//deel2
+const isFemale = true;
+if (isFemale = true) {
+    document.write("Alle vrouwen zijn welkom op ladies night, je mag verder");
+    console.log("female");
+}
+
+else if (isFemale = false) {
+    document.write("Alleen vrouwen zijn welkom op ladies night, je mag dus niet verder");
+    console.log("male");
+}
+
+//deel3
+const driverStatus = bob;
+if (driverStatus = bob) {
+    document.write("U geeft aan de bob te zijn en kunt veilig naar huis rijden");
+    console.log("bob");
+}
+
+else if (driverStatus = bob) {
+    document.write("U geeft aan alohol te hebben gedronken en kunt dus niet veilig naar huis rijden");
+    console.log("niet de bob");
+}
